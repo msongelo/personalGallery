cb7f23130028bcbdf2fb72b89a9a64662b770fd0
Zinande <zennymsongelo44@gmail.com>
2021-01-30 09:19:05 +0200

    Data folder

---
+++ Data/PersonalGalleryContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using PersonalGallery.Models;
+
+namespace PersonalGallery.Data
+{
+    public class PersonalGalleryContext : DbContext
+    {
+        public PersonalGalleryContext (DbContextOptions<PersonalGalleryContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<PersonalGallery.Models.ImageModel> ImageModel { get; set; }
+    }
+}
